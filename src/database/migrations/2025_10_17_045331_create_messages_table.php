a94d5cbe6d4ff12a77874a31c484bb51d36d9dae
tatsuki1107 <k2wnt2i@gmail.com>
2025-10-23 23:20:58 +0900

    メッセージ機能の構文エラー解消

--- src/database/migrations/2025_10_17_045331_create_messages_table.php
+++ src/database/migrations/2025_10_17_045331_create_messages_table.php
@@ -4,20 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-$table->timestamps();
-
 return new class extends Migration
 {
     public function up(): void
-{
-    Schema::create('messages', function (Blueprint $table) {
-        $table->id();
-        $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
-        $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
-        $table->text('content');
-        $table->timestamps();
-    });
-}
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
 
     /**
      * Reverse the migrations.
